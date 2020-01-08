6f8a8ba3d2987fd4d2512e29d2dabdd0ed270643
Sophie Gu <sophieqgu@gmail.com>
2020-01-08 00:45:06 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,49 +1,74 @@
 class FiguresController < ApplicationController
   # add controller methods
-  get '/figures' do
-      @figures = Figure.all
-      erb :'figures/index'
-    end
-
-    get '/figures/new' do
-      erb :'figures/new'
-    end
-
-    get '/figures/:id' do
-      @figure = Figure.find(params[:id])
-      erb :'figures/show'
-    end
-
-    get '/figures/:id/edit' do
-      @figure = Figure.find(params[:id])
-      erb :'figures/edit'
-    end
-
-    post '/figures' do
-      @figure = Figure.create(params['figure'])
-      unless params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
-
-      unless params[:title][:name].empty?
-        @figure.titles << Title.create(params[:title])
-      end
-
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
-
-    patch '/figures/:id' do
-      @figure = Figure.find_by_id(params[:id])
-      @figure.update(params[:figure])
-      unless params[:title][:name].empty?
-        @figure.titles << Title.create(params[:title])
-      end
-      unless params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'/figures/index'
+  end 
+  
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end 
+  
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end 
+  
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    
+    params[:figure][:title_ids].each do |title|
+      @figure.titles << Title.find_or_create_by(title)
+    end 
+    
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+    
+    params[:figure][:landmark_ids].each do |landmark|
+      @figure.landmarks << Landmark.find_or_create_by(landmark)
+    end 
+    
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+    
+    @figure.save
+    
+    redirect to :"figures/#{@figure.id}"
+  end 
+  
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end 
+  
+  patch '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    
+    params[:figure][:title_ids].each do |title|
+      @figure.titles << Title.find(title)
+    end 
+    
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+    
+    params[:figure][:landmark_ids].each do |landmark|
+      @figure.landmarks << Landmark.find(landmark)
+    end 
+    
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+    
+    @figure.save
+    redirect to :"figures/#{@figure.id}"
+  end 
     
 end
