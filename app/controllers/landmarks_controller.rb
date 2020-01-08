0106d1fbbdc0d3287ee82fcb01ef057d0b47e2b7
Sophie Gu <sophieqgu@gmail.com>
2020-01-08 01:28:34 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,4 +8,48 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do 
     erb :'/landmarks/new'
   end 
+  
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end 
+  
+  post '/landmarks' do 
+    @landmark = Landmark.find(params[:id])
+    
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+   
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+    
+    @figure.save
+    
+    redirect to :"figures/#{@figure.id}"
+  end 
+  
+  get '/figures/:id/edit' do 
+    @landmark = Landmark.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end 
+  
+  patch '/figures/:id' do 
+    @landmark = Landmark.find(params[:id])
+    @figure.update(params[:figure])
+    
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+    
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+    
+    @figure.save
+    redirect to :"figures/#{@figure.id}"
+  end 
 end
